4fefec17109c8497bfe08c71292ea03e1f510319
Miika Sikala <miikasikala96@gmail.com>
2016-10-06 00:09:58 +0300

    Laravel installation + basic authentication

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="jumbotron jumbotron-white">
+                <h1>Welcome to Fapp!</h1>
+
+                <p>Log in or sign up to see it's all features! It's free now and forever!</p>
+
+                <p><a class="btn btn-lg btn-primary" href="/register" role="button">Sign up</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
